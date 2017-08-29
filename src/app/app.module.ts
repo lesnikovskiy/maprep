a5a82df14c6a54e1f6b96fc83b4dbdf2f680c6a3
lesnikovskiy <lesnikovski@gmail.com>
2017-08-29 18:41:00 +0300

    add routing configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,35 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent }  from './app.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { DashboardComponent } from './dashboard.component';
 
 import { HeroService } from './hero.service';
 
 @NgModule({
 	imports:      [
 		BrowserModule,
-		FormsModule
+		FormsModule,
+		RouterModule.forRoot([
+			{
+				path: 'heroes',
+				component: HeroesComponent
+			},
+			{
+				path: 'dashboard',
+				component: DashboardComponent
+			}
+		])
 	],
 	declarations: [
 		AppComponent,
 		HeroesComponent,
-		HeroDetailComponent
+		HeroDetailComponent,
+		DashboardComponent
 	],
 	providers: [
 		HeroService
--- src/app/dashboard.component.js
+++ src/app/dashboard.component.js
@@ -6,17 +6,16 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 var core_1 = require("@angular/core");
-var AppComponent = (function () {
-    function AppComponent() {
-        this.title = 'Tour of Heroes';
+var DashboardComponent = (function () {
+    function DashboardComponent() {
     }
-    return AppComponent;
+    return DashboardComponent;
 }());
-AppComponent = __decorate([
+DashboardComponent = __decorate([
     core_1.Component({
-        selector: 'my-app',
-        template: "\n\t\t<h1>{{title}}</h1>\n\t\t<a routerLink=\"/heroes\">Heroes</a>\n\t\t<a routerLink=\"/dashboard\">Dashboard</a>\n\t\t<router-outlet></router-outlet>\n\t"
+        selector: 'my-dashboard',
+        template: '<h3>My Dashboard</h3>'
     })
-], AppComponent);
-exports.AppComponent = AppComponent;
-//# sourceMappingURL=app.component.js.map
+], DashboardComponent);
+exports.DashboardComponent = DashboardComponent;
+//# sourceMappingURL=dashboard.component.js.map
